cc1d873b0f19a4b93d48b46263b1fbac1f0e8022
therunningagent <luistjr@gmail.com>
2020-12-09 17:03:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(name, event) {
+    let thankYouCards =[]
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number) {
+    let i = 10;
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number); 
+}
